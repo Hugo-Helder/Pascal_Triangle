2dbf117f7d1e6abf847c73a28073801a26c5b91d
Hugo-Helder <59727184+Hugo-Helder@users.noreply.github.com>
2025-03-03 16:20:38 +0100

    Added big number to demonstrate effiency of O(n) solution (time and space)

--- src/test/java/nl/hu/xe/xebia/XebiaApplicationTest.java
+++ src/test/java/nl/hu/xe/xebia/XebiaApplicationTest.java
@@ -1,7 +1,5 @@
 package nl.hu.xe.xebia;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -27,7 +25,8 @@ class XebiaApplicationTest {
                 Arguments.of(1, List.of(1, 1)),
                 Arguments.of(2, List.of(1, 2, 1)),
                 Arguments.of(3, List.of(1, 3, 3, 1)),
-                Arguments.of(4, List.of(1, 4, 6, 4, 1))
+                Arguments.of(4, List.of(1, 4, 6, 4, 1)),
+                Arguments.of(20, List.of(1, 20, 190, 1140, 4845, 15504, 38760, 77520, 125970, 167960, 184756, 167960, 125970, 77520 , 38760, 15504, 4845, 1140, 190, 20, 1))
 
         );
     }
