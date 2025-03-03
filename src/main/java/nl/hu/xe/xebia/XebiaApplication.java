f8c79b6c908ccc0df7c034c51e71c8d34ba61c88
Hugo-Helder <59727184+Hugo-Helder@users.noreply.github.com>
2025-03-03 15:36:43 +0100

    Created solution for pascal triangle problem

---
+++ src/main/java/nl/hu/xe/xebia/XebiaApplication.java
@@ -0,0 +1,13 @@
+package nl.hu.xe.xebia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XebiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XebiaApplication.class, args);
+    }
+
+}
