29990d0b2084d69c217f6705bb0f977cf35b9e0d
Hugo-Helder <59727184+Hugo-Helder@users.noreply.github.com>
2025-03-03 15:44:50 +0100

    Added explanation

--- src/main/java/nl/hu/xe/xebia/XebiaApplication.java
+++ src/main/java/nl/hu/xe/xebia/XebiaApplication.java
@@ -3,6 +3,9 @@ package nl.hu.xe.xebia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class XebiaApplication {
 
@@ -10,4 +13,40 @@ public class XebiaApplication {
         SpringApplication.run(XebiaApplication.class, args);
     }
 
+    /**
+     * @return
+     *         nCi = n! / (i! * (n-i)!)   – ith element of nth row
+     *         nCi+1 = n! / ((i+1)! * (n-(i+1))! )  – (i+1)th element of nth row
+     *         Dividing  (i+1)th element by ith element will give:
+     *         nCi+1 / nCi = i! * (n-i)! / (i+1)! * (n-i-1)!  – On solving this
+     *         nCi+1 = nCi  * (n-i) / (i+1) –  we get (i+1)th element with the help of ith element
+     */
+    List<Integer> triangleRow(int row) {
+        ArrayList<Integer> pascalRow = new ArrayList<>();
+
+        if (row < 0) {
+            return List.of();
+        }
+        int previousValue = 1;
+        pascalRow.add(previousValue);
+
+        for (int i = 1; i <= row; i++) {
+
+            // nCr = (nCr-1 * (n - r + 1))/r
+            // https://www.geeksforgeeks.org/ncr-formula/
+            // only generate numbers for the row specified
+            int nCr = (previousValue * (row - i + 1)) / i;
+            pascalRow.add(nCr);
+            previousValue = nCr;
+        }
+        return pascalRow;
+    }
+
 }
+
+
+
+
+
+
+
--- src/test/java/nl/hu/xe/xebia/XebiaApplicationTest.java
+++ src/test/java/nl/hu/xe/xebia/XebiaApplicationTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nl.hu.xe.xebia;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class XebiaApplicationTest {
-  
+    XebiaApplication x = new XebiaApplication();
+
+    @ParameterizedTest
+    @MethodSource("testdata")
+    void triangleRow(int rowNumber, List<Integer> result) {
+        assertEquals(result, x.triangleRow(rowNumber));
+    }
+
+    static Stream<Arguments> testdata() {
+        return Stream.of(
+                Arguments.of(-1, List.of()),
+                Arguments.of(0, List.of(1)),
+                Arguments.of(1, List.of(1, 1)),
+                Arguments.of(2, List.of(1, 2, 1)),
+                Arguments.of(3, List.of(1, 3, 3, 1)),
+                Arguments.of(4, List.of(1, 4, 6, 4, 1))
+
+        );
+    }
 }
